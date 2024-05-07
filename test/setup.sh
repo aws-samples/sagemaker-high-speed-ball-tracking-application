c174996faa1bc356a476154257c93d8b4cfdc9d3
troy.dieter <troydieter@gmail.com>
2024-05-07 11:49:06 -0400

    revised setup.sh for both apt and yum

--- test/setup.sh
+++ test/setup.sh
@@ -2,4 +2,13 @@
 
 pip install -r requirements.txt
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
-sudo yum install mesa-libGL -y
+# sudo yum install mesa-libGL -y
+if command -v yum &> /dev/null; then
+    sudo yum install mesa-libGL -y
+elif command -v apt-get &> /dev/null; then
+    sudo apt-get update && sudo apt-get install ffmpeg libsm6 libxext6 -y
+    sudo apt-get install libgl1-mesa-dri -y
+else
+    echo "Neither apt nor yum package managers are available."
+    exit 1
+fi
